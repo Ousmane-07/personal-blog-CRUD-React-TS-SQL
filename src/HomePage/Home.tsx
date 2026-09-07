599ca3ace174c1d81ef6786975b4a6a46007e08f
Ousmane <ousmanegitgit@gmail.com>
2026-09-07 02:51:07 -0400

    top half of home component header and paragraph portion complete

--- src/HomePage/Home.tsx
+++ src/HomePage/Home.tsx
@@ -1,10 +1,26 @@
-
+import './Home.css'
 
 function Home() { 
 
     return ( 
         <>
-        Home
+        <div className="home-wrapper">
+            <div className="home-container">
+                 <h1>Hi, I'm Paulina👋 </h1>
+                 <p>I’m on a journey to become a front-end web developer.
+                     I love building little projects, trying out new coding
+                     techniques, and sharing what I learn along the way.
+                     When I’m not at my desk, you’ll find me reading,
+                     hiking through the mountains, or challenging myself 
+                     on rock-climbing walls.
+                     <br></br>
+                     <br></br>
+                     I started this blog to document my progress, keep 
+                     myself accountable, and hopefully inspire anyone 
+                     else who’s learning to code. Welcome to my corner 
+                     of the internet, and thanks for stopping by!</p>
+            </div>
+        </div>
         </>
     )
 }
