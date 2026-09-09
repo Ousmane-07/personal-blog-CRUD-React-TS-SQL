3da6ad76a0ce7ec04161e5ee50922a074769de80
Ousmane <ousmanegitgit@gmail.com>
2026-09-08 21:33:46 -0400

    added database server for communicating with express

---
+++ server/db.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({ 
+    user: "postgres",
+    host: "localhost",
+    database: "personal_blog",
+    password: "Ou$mane76037603",
+    port: 5432,
+})
+
+export default pool
